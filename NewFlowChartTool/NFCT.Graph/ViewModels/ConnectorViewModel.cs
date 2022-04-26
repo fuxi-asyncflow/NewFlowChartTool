fa6313060419c35d5608d9329d6f5b3634fee7ed
zqw10 <zqw1989@outlook.com>
2022-04-26 15:17:32 +0800

    draw arrow for curve

--- NewFlowChartTool/NFCT.Graph/ViewModels/ConnectorViewModel.cs
+++ NewFlowChartTool/NFCT.Graph/ViewModels/ConnectorViewModel.cs
@@ -28,9 +28,42 @@ namespace NFCT.Graph.ViewModels
             set => CreatePath(value);
         }
 
+        private static PathFigure _genArrow(Position s, Position e, double w = 4.0, double h = 8.0)
+        {
+            var p2 = e;
+            var p1 = s;
+            double dx = p2.x - p1.x;
+            double dy = p2.y - p1.y;
+
+            double l = Math.Sqrt(dx * dx + dy * dy);
+            double offsetX = -dy * w / l;
+            double offsetY = dx * w / l;
+
+
+            var seg = new LineSegment();
+
+            Point[] arrowPoints = new Point[3];
+            arrowPoints[0].X = p2.x + offsetX - h / l * dx;
+            arrowPoints[0].Y = p2.y + offsetY - h / l * dy;
+            //arrowPoints[3].X = p2.X + offsetX;
+            //arrowPoints[3].Y = p2.Y + offsetY;
+
+            arrowPoints[2].X = p2.x - offsetX - h / l * dx;
+            arrowPoints[2].Y = p2.y - offsetY - h / l * dy;
+
+            arrowPoints[1].X = p2.x;
+            arrowPoints[1].Y = p2.y;
+
+
+            PathFigure figure = new PathFigure(arrowPoints[0], new []{ new LineSegment(arrowPoints[1], true), new LineSegment(arrowPoints[2], true)}, true);
+            return figure;
+        }
+
         public void CreatePath(List<Curve> curves)
         {
             PathGeometry geometry = new PathGeometry();
+            if (curves.Count == 0)
+                return;
 
             // Console.WriteLine("=================");
             foreach (var curve in curves)
@@ -63,7 +96,17 @@ namespace NFCT.Graph.ViewModels
                 
             }
 
-            
+            var endCurve = curves.Last();
+            if (endCurve.Type == Curve.CurveType.Line)
+            {
+                geometry.Figures.Add(_genArrow(endCurve.Points[0], endCurve.Points[1]));
+            }
+            else if(endCurve.Type == Curve.CurveType.SPLINE)
+            {
+                int pointCount = endCurve.Points.Count;
+                geometry.Figures.Add(_genArrow(endCurve.Points[pointCount-2], endCurve.Points[pointCount-1]));
+            }
+
             Path = geometry;
             RaisePropertyChanged(nameof(Path));
         }
