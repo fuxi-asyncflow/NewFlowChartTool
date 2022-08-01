6e5207e2f6f978a78c952b2dee0a78be8521b442
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2022-08-01 20:15:56 +0800

    [editor] add actb control

---
+++ NewFlowChartTool/NFCT.Graph/ViewModels/NodeAutoCompleteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace NFCT.Graph.ViewModels
+{
+    public class NodeAutoCompleteViewModel : BindableBase
+    {
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set => SetProperty(ref _text, value, nameof(Text));
+        }
+    }
+}
