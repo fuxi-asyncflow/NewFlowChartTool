3cc6df6d6d6018dbc6787b9536f8915d279ff108
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2022-08-30 19:27:03 +0800

    [editor] project tree style

--- NewFlowChartTool/NFCT.Common/Converter.cs
+++ NewFlowChartTool/NFCT.Common/Converter.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using NFCT.Common.Misc;
 
 namespace NFCT.Common
 {
@@ -30,6 +32,26 @@ namespace NFCT.Common
                 return true;
             }
             return false;
+        }
+    }
+
+    [ValueConversion(typeof(TreeViewItem), typeof(Thickness))]
+    public class LeftMarginMultiplierConverter : IValueConverter
+    {
+        public double Length { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value as TreeViewItem;
+            if (item == null)
+                return new Thickness(0);
+
+            return new Thickness(Length * item.GetDepth(), 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
