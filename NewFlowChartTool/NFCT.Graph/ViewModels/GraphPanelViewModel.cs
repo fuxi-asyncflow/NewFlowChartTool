27bcc67b42bd7d11239d42dcef85e836833092e7
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2022-09-15 16:21:45 +0800

    [refactor] split files

--- NewFlowChartTool/NFCT.Graph/ViewModels/GraphPanelViewModel.cs
+++ NewFlowChartTool/NFCT.Graph/ViewModels/GraphPanelViewModel.cs
@@ -15,7 +15,6 @@ using FlowChartCommon;
 using NFCT.Common;
 using NFCT.Common.Events;
 using NFCT.Common.ViewModels;
-using NLog.Fluent;
 using Prism.Commands;
 
 namespace NFCT.Graph.ViewModels
@@ -116,66 +115,13 @@ namespace NFCT.Graph.ViewModels
             return ConnectorDict[conn];
         }
         
-        public bool NeedLayout { get; set; }
-        public bool IsFirstLayout { get; set; }
-        private double _width;
-        public double Width { get => _width; set => SetProperty(ref _width, value); }
-
-        public double _height;
-        public double Height { get => _height; set => SetProperty(ref _height, value); }
-
-        public bool AutoLayout
-        {
-            get => _graph.AutoLayout;
-        }
+        
 
         public DelegateCommand ChangeLayoutCommand { get; set; }
         public DelegateCommand CreateGroupCommand { get; set; }
         public DelegateCommand OnCloseCommand { get; set; }
 
-        public void ChangeAutoLayout()
-        {
-            if (AutoLayout)
-            {
-                _graph.AutoLayout = false;
-                RaisePropertyChanged(nameof(AutoLayout));
-            }
-            else
-            {
-                var result = MessageBox.Show("Switch to AutoLayout, all nodes position will change", "Be careful",
-                    MessageBoxButton.YesNo);
-                if (result == MessageBoxResult.No)
-                    return;
-                _graph.AutoLayout = true;
-                NeedLayout = true;
-                RaisePropertyChanged(nameof(AutoLayout));
-            }
-        }
-
-        public bool Relayout()
-        {
-            Logger.DBG($"Relayout for graph {Name}");
-            var graph = new GraphLayoutAdapter(this);
-            if (NodeDict.Count <= 0.0 || NodeDict.First().Value.Width <= 0.0)
-                return false;
-            MsaglLayout layout = new MsaglLayout();
-            try
-            {
-                layout.Layout(graph);
-                if (Height < 0.001 || Width < 0.001)
-                {
-                    return false;
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.ERR($"[error] layout failed {e.Message}");
-                return false;
-            }
-
-            return true;
-        }
-
+        
         public void Build()
         {
             Graph.Build(new ParserConfig(){GetTokens = true});
---
+++ NewFlowChartTool/NFCT.Graph/ViewModels/GraphPanelViewModel_Layout.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FlowChart.Layout;
+using FlowChartCommon;
+
+namespace NFCT.Graph.ViewModels
+{
+    public partial class GraphPaneViewModel
+    {
+        public bool NeedLayout { get; set; }
+        public bool IsFirstLayout { get; set; }
+        private double _width;
+        public double Width { get => _width; set => SetProperty(ref _width, value); }
+
+        public double _height;
+        public double Height { get => _height; set => SetProperty(ref _height, value); }
+
+        public bool AutoLayout
+        {
+            get => _graph.AutoLayout;
+        }
+
+        public void ChangeAutoLayout()
+        {
+            if (AutoLayout)
+            {
+                _graph.AutoLayout = false;
+                RaisePropertyChanged(nameof(AutoLayout));
+            }
+            else
+            {
+                var result = MessageBox.Show("Switch to AutoLayout, all nodes position will change", "Be careful",
+                    MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.No)
+                    return;
+                _graph.AutoLayout = true;
+                NeedLayout = true;
+                RaisePropertyChanged(nameof(AutoLayout));
+            }
+        }
+
+        public bool Relayout()
+        {
+            Logger.DBG($"Relayout for graph {Name}");
+            var graph = new GraphLayoutAdapter(this);
+            if (NodeDict.Count <= 0.0 || NodeDict.First().Value.Width <= 0.0)
+                return false;
+            MsaglLayout layout = new MsaglLayout();
+            try
+            {
+                layout.Layout(graph);
+                if (Height < 0.001 || Width < 0.001)
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.ERR($"[error] layout failed {e.Message}");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
