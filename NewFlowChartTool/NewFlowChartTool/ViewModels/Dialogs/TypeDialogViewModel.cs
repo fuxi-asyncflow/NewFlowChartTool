48c2d505b82f9988d392c1fd62f1797e2625bebe
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2022-10-23 23:34:45 +0800

    [editor] WIP: type dialog

--- NewFlowChartTool/NewFlowChartTool/ViewModels/Dialogs/TypeDialogViewModel.cs
+++ NewFlowChartTool/NewFlowChartTool/ViewModels/Dialogs/TypeDialogViewModel.cs
@@ -18,6 +18,13 @@ using Type = FlowChart.Type.Type;
 
 namespace NewFlowChartTool.ViewModels
 {
+    class ParameterViewModel : BindableBase
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string? Description { get; set; }
+        public string? DefaultValue { get; set; }
+    }
     class TypeMemberViewModel : BindableBase
     {
         public TypeMemberViewModel(Member member)
@@ -28,6 +35,7 @@ namespace NewFlowChartTool.ViewModels
         public string Name => Model.Name;
         public string Type => Model.Type.Name;
         public string? Description => Model.Description;
+        public bool IsVariadic;
 
         public string? Parameters
         {
@@ -75,10 +83,14 @@ namespace NewFlowChartTool.ViewModels
         {
             Types = new ObservableCollection<TypeViewModel>();
             Members = new ObservableCollection<TypeMemberViewModel>();
+            Parameters = new ObservableCollection<ParameterViewModel>();
             _outputService = outputService;
             EventHelper.Sub<ProjectCloseEvent, Project>(OnCloseProject, ThreadOption.UIThread);
 
             SelectedTypeChangeCommand = new DelegateCommand(OnSelectedTypeChange);
+            SelectedMemberChangeCommand = new DelegateCommand(OnSelectedMemberChange);
+            AddNewTypeCommand = new DelegateCommand(AddNewType);
+            SaveCommand = new DelegateCommand(Save);
         }
 
         private readonly IOutputMessage _outputService;
@@ -124,12 +136,15 @@ namespace NewFlowChartTool.ViewModels
         public event Action<IDialogResult>? RequestClose;
         #endregion
 
-        public TypeViewModel? SelectedType { get; set; }
+        private TypeViewModel? _selectedType;
+        public TypeViewModel? SelectedType { get => _selectedType; set => SetProperty(ref _selectedType, value); }
         public ObservableCollection<TypeViewModel> Types { get; set; }
-        public TypeMemberViewModel SelectedMember { get; set; }
+        public TypeMemberViewModel? SelectedMember { get; set; }
         public ObservableCollection<TypeMemberViewModel> Members { get; set; }
         public DelegateCommand SelectedTypeChangeCommand { get; set; }
-
+        public DelegateCommand SelectedMemberChangeCommand { get; set; }
+        public DelegateCommand AddNewTypeCommand { get; set; }
+        public DelegateCommand SaveCommand { get; set; }
 
         void OnCloseProject(Project project)
         {
@@ -143,6 +158,7 @@ namespace NewFlowChartTool.ViewModels
                 return;
             Output($"type change to {SelectedType.Name}");
             Members.Clear();
+            SelectedMember = null;
             foreach (var kv in SelectedType.Model.MemberDict)
             {
                 Members.Add(new TypeMemberViewModel(kv.Value));
@@ -150,5 +166,91 @@ namespace NewFlowChartTool.ViewModels
             
         }
 
+        #region Member Editor
+
+        private string _memberName;
+        public string MemberName { get => _memberName; set => SetProperty(ref _memberName, value); }
+        private string? _memberDescription;
+        public string? MemberDescription { get => _memberDescription; set => SetProperty(ref _memberDescription, value); }
+        private string _memberType;
+        public string MemberType { get => _memberType; set => SetProperty(ref _memberType, value); }
+
+        public ObservableCollection<ParameterViewModel> Parameters { get; set; }
+
+        #endregion
+
+        void OnSelectedMemberChange()
+        {
+            if (SelectedMember == null)
+            {
+                MemberName = string.Empty;
+                MemberDescription = null;
+                MemberType = string.Empty;
+                Parameters.Clear();
+                return;
+            }
+
+            var m = SelectedMember;
+            MemberName = m.Name;
+            MemberDescription = m.Description;
+            MemberType = m.Type;
+            Parameters.Clear();
+            if (m.Model is Method method)
+            {
+                method.Parameters.ForEach(p =>
+                {
+                    var pvm = new ParameterViewModel
+                    {
+                        Name = m.Name,
+                        Description = m.Description,
+                        DefaultValue = p.Default
+                    };
+                    Parameters.Add(pvm);
+                });
+            }
+        }
+
+        string? GetNewTypeName()
+        {
+            if (CurrentProject == null)
+                return null;
+            const string NEW_TYPE_NAME = "NewType";
+            var name = NEW_TYPE_NAME;
+            if (CurrentProject.GetType(name) == null)
+                return name;
+            for (int i = 0; i < 100; i++)
+            {
+                name = $"{NEW_TYPE_NAME}_{i}";
+                if (CurrentProject.GetType(name) == null)
+                    return name;
+            }
+            return null;
+        }
+
+        void AddNewType()
+        {
+            var newTypeName = GetNewTypeName();
+            if (newTypeName == null)
+                return;
+            var newType = new Type(newTypeName);
+            var newTypeVm = new TypeViewModel(newType);
+            Types.Add(newTypeVm);
+            SelectedType = newTypeVm;
+            OnSelectedTypeChange();
+        }
+
+        void Save()
+        {
+            if (CurrentProject == null)
+                return;
+            foreach (var typeVm in Types)
+            {
+                if(CurrentProject.GetType(typeVm.Name) != null)
+                    continue;
+                CurrentProject.AddType(typeVm.Model);
+                Output($"type {typeVm.Name} successfully added");
+            }
+            CurrentProject.Save();
+        }
     }
 }
