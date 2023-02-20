3221733ba26b1dc5de5efd87f4089467ea540217
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2023-02-20 19:53:06 +0800

    [editor] change variables pane by mouse drag

--- NewFlowChartTool/NFCT.Graph/Views/GraphVariablesPanel.xaml.cs
+++ NewFlowChartTool/NFCT.Graph/Views/GraphVariablesPanel.xaml.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using FlowChart.Common;
 using NFCT.Graph.ViewModels;
 
 namespace NFCT.Graph.Views
@@ -18,6 +20,44 @@ namespace NFCT.Graph.Views
         public GraphVariablesPanel()
         {
             InitializeComponent();
+        }
+
+
+        private bool _isResizing;
+        private Point _startPoint;
+        private double _originWidth;
+        private double _originHeight;
+        private void ResizeCorner_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isResizing && e.LeftButton == MouseButtonState.Pressed)
+            {
+                var curPos = e.GetPosition(VariablesPaneGrid);
+                var deltaPos = curPos - _startPoint;
+                VariablesPaneGrid.Width = _originWidth + deltaPos.X;
+                VariablesPaneGrid.Height = _originHeight + deltaPos.Y;
+            }
+        }
+
+        private void ResizeCorner_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isResizing = true;
+            Logger.DBG("begin resize variables pane");
+            _startPoint = e.GetPosition(VariablesPaneGrid);
+            _originWidth = VariablesPaneGrid.Width;
+            _originHeight = VariablesPaneGrid.Height;
+        }
+
+        private void ResizeCorner_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isResizing = false;
+            Logger.DBG("exit resize variables pane");
+
+        }
+
+        private void ResizeCorner_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            _isResizing = false;
+            Logger.DBG("exit resize variables pane");
         }
     }
 }
